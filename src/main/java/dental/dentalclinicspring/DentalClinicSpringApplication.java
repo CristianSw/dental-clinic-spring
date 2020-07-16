8050ca8471f637e104e3c8a3e76f84a173a9f876
Cristian Dolinţă <cristian@pop-os.localdomain>
2020-07-16 12:38:17 +0300

    Init Project Structure

---
+++ src/main/java/dental/dentalclinicspring/DentalClinicSpringApplication.java
@@ -0,0 +1,13 @@
+package dental.dentalclinicspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DentalClinicSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DentalClinicSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dental/dentalclinicspring/DentalClinicSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package dental.dentalclinicspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DentalClinicSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
